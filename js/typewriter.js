4c357e81887a4dc2ff339e085d75722287e1bf4f
Joshua Sachtleben <joshuasachtleben@gmail.com>
2015-01-17 17:06:03 -0500

    Updated JavaScript to write to one element ID.  Script will now type one letter at a time.

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,7 +1,14 @@
-var elements = document.getElementsByTagName("*");
-var count = elements.length;
-for (var i = 0; i < count; i++) {
-    if(elements[i].className.indexOf("typewriter") > -1) {
-        alert("Typewriter class found!");
+//get original text from element
+var text = document.getElementById("typewriter").innerHTML;
+var textCount = text.length;
+//clear the element so we can type it out
+document.getElementById("typewriter").innerHTML = "";
+var i = 0;
+//create a variable of the setInterval function so we can call it during clearInterval if the i counter hits the end of the text length.  Otherwise, this will run infinitely.
+var typeText = setInterval(function () {
+    document.getElementById("typewriter").innerHTML += text.substring(i, i + 1);
+    i++;
+    if (i >= textCount) {
+        clearInterval(typeText);
     }
-}
+}, 80);  //200ms.  This can be lowered to type faster.
